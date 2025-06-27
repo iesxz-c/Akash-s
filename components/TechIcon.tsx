c2ebc7634a99b984a60fe6def8a4ca3464ff958c
Akash <akash.krishnan.046@gmail.com>
2025-06-27 20:02:48 +0530

    err @ interface comp

--- components/TechIcon.tsx
+++ components/TechIcon.tsx
@@ -1,18 +1,22 @@
-export const TechIcon = ({ component }: { component: React.ElementType }) => {
-  const Component = component;
-  return <>
-  <Component className ="size-10 fill-[url(#gradient)] "/>
-  <svg className="size-10 absolute ">
-    <linearGradient id="gradient">
-      <stop offset="0%" stopColor="rgb(236 72 153)" />
-      <stop offset="100%" stopColor="#48484a"/>
-      
-    </linearGradient>
-  </svg>
-  
-  
-  </>;
+import React from 'react';
+
+// Option 1: More specific typing (recommended)
+interface TechIconProps {
+  component: React.ComponentType<{ className?: string }>;
+}
+
+export const TechIcon = ({ component: Component }: TechIconProps) => {
+  return (
+    <>
+      <Component className="size-10 fill-[url(#gradient)]" />
+      <svg className="size-10 absolute">
+        <defs>
+          <linearGradient id="gradient">
+            <stop offset="0%" stopColor="rgb(236 72 153)" />
+            <stop offset="100%" stopColor="#48484a" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </>
+  );
 };
-
-
- 
